5ad4b9e1b1ffc1f8c3b3c1ad40cc893e1255c81b
Koudjo AHOLOU <ah.koudjo@gmail.com>
2020-10-28 11:45:21 +0100

    ajout d un loading correction bug build + cmd deploy

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 it('Should display the element while allow to start the game', () => {
-  const { getByText } = render(<App />);
-  const buttonElement = getByText(/^Commencer une partie/i, { selector: 'button.commencerTexte' });
-  expect(buttonElement).toBeInTheDocument();
+  const { container } = render(<App />);
+  const spinner = container.querySelector('article.spinner');
+  expect(spinner).toBeInTheDocument();
 });
