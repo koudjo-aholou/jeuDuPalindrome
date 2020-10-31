8d2718c921b8a58ddc34f5bf1bd6c8c4a65be5e9
Koudjo AHOLOU <ah.koudjo@gmail.com>
2020-10-31 19:52:26 +0100

    ajout composant Bonux

---
+++ src/component/game/Bonux.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Bonux = () => (
+  <div className="showhim">
+    <div id="bonux">Score *2</div>
+  </div>
+);
+
+export default Bonux;
+
+// const x = document.getElementById('showhim');
+
+// var y = setInterval(function(){
+//   x.style.left = `${Math.ceil(Math.random()*60)}vh`;
+//   x.style.top = `${Math.ceil(Math.random()*40)}vh`;
+// }, 2000);
+
+// x.onclick = termine
+// function termine(){
+//   console.log('ici=====')
+//   clearInterval(y);
+// }
